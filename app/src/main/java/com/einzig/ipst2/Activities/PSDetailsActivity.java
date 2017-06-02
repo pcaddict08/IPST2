238bb3abe6d8d7b5e9d2ebe398ba2396509abbe6
Steven Foskett <steven.foskett@gmail.com>
2017-06-02 15:59:32 -0500

    Homogenized Portal Details info boxes, added rejected and accepted boxes.

--- app/src/main/java/com/einzig/ipst2/Activities/PSDetailsActivity.java
+++ app/src/main/java/com/einzig/ipst2/Activities/PSDetailsActivity.java
@@ -25,11 +25,12 @@ package com.einzig.ipst2.activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -48,6 +49,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.einzig.ipst2.DialogHelper;
 import com.einzig.ipst2.R;
 import com.einzig.ipst2.portal.PortalAccepted;
 import com.einzig.ipst2.portal.PortalRejected;
@@ -63,7 +65,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.UUID;
 
 public class PSDetailsActivity extends AppCompatActivity {
     PortalSubmission portalSubmission;
@@ -104,40 +105,60 @@ public class PSDetailsActivity extends AppCompatActivity {
         }
     }
 
-    public void buildUI()
-    {
+    public void buildUI() {
         Log.d(MainActivity.TAG, "PS Type: " + portalSubmission.getClass().getName());
         ((TextView) findViewById(R.id.name_psdetailsactivity)).setText(portalSubmission.getName());
         ((TextView) findViewById(R.id.submitted_psdetailsactivity)).setText(portalSubmission.getSubmittedDateString());
-        if (portalSubmission instanceof PortalResponded) {
-            ((TextView) findViewById(R.id.daysinqueue_psdetailsactivity)).setText(String.valueOf(((PortalResponded) portalSubmission).getResponseTime()));
-            LinearLayout respondedLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.row_psdetails_responded, (LinearLayout) findViewById(R.id.extralayout_psdetailsactivity), false);
-            ((TextView)respondedLayout.findViewById(R.id.dateresponded_respondedrow)).setText(((PortalResponded) portalSubmission).getDateRespondedString());
-            ((LinearLayout) findViewById(R.id.extralayout_psdetailsactivity)).addView(respondedLayout);
-            if (portalSubmission instanceof PortalRejected) {
-                findViewById(R.id.psstatusimage_psdetailsactivity).setBackgroundColor(getResources().getColor(R.color.rejected));
-                ((ImageView) findViewById(R.id.psstatusimage_psdetailsactivity)).setImageDrawable(getResources().getDrawable(R.drawable.ic_rejected));
-            } else if (portalSubmission instanceof PortalAccepted) {
-                findViewById(R.id.psstatusimage_psdetailsactivity).setBackgroundColor(getResources().getColor(R.color.accepted));
-                ((ImageView) findViewById(R.id.psstatusimage_psdetailsactivity)).setImageDrawable(getResources().getDrawable(R.drawable.ic_check));
-            }
-        }
-        else
-            ((TextView) findViewById(R.id.daysinqueue_psdetailsactivity)).setText(String.valueOf(portalSubmission.getDaysSinceResponse()));
-
-        setUpImageDownloadButton();
-
-        //Loading image into imageview
         Picasso.with(this)
                 .load(portalSubmission.getPictureURL())
                 .error(R.drawable.ic_warning_white)
                 .into(((ImageView) findViewById(R.id.psimage_psdetailsactivity)));
-
+        setUpExtraUI();
+        setUpImageDownloadButton();
         setUpImageDetailsView();
     }
 
-    public void setUpImageDownloadButton()
-    {
+    public void setUpExtraUI() {
+        if (portalSubmission instanceof PortalResponded) {
+            ((TextView) findViewById(R.id.daysinqueue_psdetailsactivity)).setText(String.valueOf(((PortalResponded) portalSubmission).getResponseTime()));
+            LinearLayout respondedLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.row_psdetails_responded, (LinearLayout) findViewById(R.id.extralayout_psdetailsactivity), false);
+            ((TextView) respondedLayout.findViewById(R.id.dateresponded_respondedrow)).setText(((PortalResponded) portalSubmission).getDateRespondedString());
+            ((LinearLayout) findViewById(R.id.extralayout_psdetailsactivity)).addView(respondedLayout);
+            if (portalSubmission instanceof PortalRejected) {
+                findViewById(R.id.psstatusimage_psdetailsactivity).setBackgroundColor(getResources().getColor(R.color.rejected));
+                ((ImageView) findViewById(R.id.psstatusimage_psdetailsactivity)).setImageDrawable(getResources().getDrawable(R.drawable.ic_rejected));
+                LinearLayout rejectedLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.row_psdetails_rejected, (LinearLayout) findViewById(R.id.extralayout_psdetailsactivity), false);
+                ((TextView) rejectedLayout.findViewById(R.id.rejectionreason_rejectedrow)).setText(((PortalRejected) portalSubmission).getRejectionReason());
+                ((LinearLayout) findViewById(R.id.extralayout_psdetailsactivity)).addView(rejectedLayout);
+            } else if (portalSubmission instanceof PortalAccepted) {
+                findViewById(R.id.psstatusimage_psdetailsactivity).setBackgroundColor(getResources().getColor(R.color.accepted));
+                ((ImageView) findViewById(R.id.psstatusimage_psdetailsactivity)).setImageDrawable(getResources().getDrawable(R.drawable.ic_check));
+                LinearLayout acceptedLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.row_psdetails_accepted, (LinearLayout) findViewById(R.id.extralayout_psdetailsactivity), false);
+                ((TextView) acceptedLayout.findViewById(R.id.liveaddress_acceptedrow)).setText(((PortalAccepted) portalSubmission).getLiveAddress());
+                acceptedLayout.findViewById(R.id.viewonintelmapbutton_acceptedrow).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goToIntel();
+                    }
+                });
+                ((LinearLayout) findViewById(R.id.extralayout_psdetailsactivity)).addView(acceptedLayout);
+            }
+        } else
+            ((TextView) findViewById(R.id.daysinqueue_psdetailsactivity)).setText(String.valueOf(portalSubmission.getDaysSinceResponse()));
+    }
+
+    public void goToIntel() {
+        try {
+            Uri uri = Uri.parse(((PortalAccepted) portalSubmission).getIntelLinkURL());
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } catch (Exception e) {
+            DialogHelper.showSimpleDialog(R.string.errortitle_viewintellink, R.string.errormessage_viewintellink, this);
+            e.printStackTrace();
+        }
+    }
+
+    public void setUpImageDownloadButton() {
         findViewById(R.id.saveportalimage_psdetailsactivity).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -151,8 +172,7 @@ public class PSDetailsActivity extends AppCompatActivity {
         });
     }
 
-    public void setUpImageDetailsView()
-    {
+    public void setUpImageDetailsView() {
         final DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         findViewById(R.id.psimage_psdetailsactivity).setOnClickListener(new View.OnClickListener() {
@@ -199,33 +219,34 @@ public class PSDetailsActivity extends AppCompatActivity {
                                     Toast.makeText(context, "Image Save Failed", Toast.LENGTH_LONG).show();
                                 }
                             });
-                        }
-                        try {
-                            FileOutputStream fos = new FileOutputStream(pictureFile);
-                            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
-                            fos.close();
-                            PSDetailsActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(context, "Image Saved - " + pictureFile.getPath(), Toast.LENGTH_LONG).show();
-                                }
-                            });
-                        } catch (FileNotFoundException e) {
-                            PSDetailsActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(context, "Image Save Failed", Toast.LENGTH_LONG).show();
-                                }
-                            });
-                            Log.d(MainActivity.TAG, "File not found: " + e.getMessage());
-                        } catch (IOException e) {
-                            PSDetailsActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(context, "Image Save Failed", Toast.LENGTH_LONG).show();
-                                }
-                            });
-                            Log.d(MainActivity.TAG, "Error accessing file: " + e.getMessage());
+                        } else {
+                            try {
+                                FileOutputStream fos = new FileOutputStream(pictureFile);
+                                bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                                fos.close();
+                                PSDetailsActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(context, "Image Saved - " + pictureFile.getPath(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            } catch (FileNotFoundException e) {
+                                PSDetailsActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(context, "Image Save Failed", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                                Log.d(MainActivity.TAG, "File not found: " + e.getMessage());
+                            } catch (IOException e) {
+                                PSDetailsActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(context, "Image Save Failed", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                                Log.d(MainActivity.TAG, "Error accessing file: " + e.getMessage());
+                            }
                         }
                     }
                 }).start();
@@ -250,28 +271,22 @@ public class PSDetailsActivity extends AppCompatActivity {
                 .into(target);
     }
 
-    /** Create a File for saving an image */
-    private  File getOutputMediaFile(){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
+    /**
+     * Create a File for saving an image
+     */
+    private File getOutputMediaFile() {
         File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
                 + "/Android/data/"
                 + getApplicationContext().getPackageName()
                 + "/Files");
-
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
                 return null;
             }
         }
-        // Create a media file name
         String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm", Locale.getDefault()).format(new Date());
         File mediaFile;
-        String mImageName="MI_"+ timeStamp +".jpg";
+        String mImageName = "MI_" + timeStamp + ".jpg";
         mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
         return mediaFile;
     }
