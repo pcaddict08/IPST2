ecdb05059c5006d8a74dd0593317436d6e17aefc
Steven Foskett <steven.foskett@gmail.com>
2017-06-06 10:03:45 -0500

    Removed Jimmy from Copyright
    Jimmy won't be contributing.

--- app/src/main/java/com/einzig/ipst2/DialogHelper.java
+++ app/src/main/java/com/einzig/ipst2/DialogHelper.java
@@ -2,7 +2,7 @@
  *  ********************************************************************************************** *
  *  * ********************************************************************************************** *
  *  *                                                                                                *
- *  * Copyright 2017 Steven Foskett, Jimmy Ho, Ryan Porterfield                                      *
+ *  * Copyright 2017 Steven Foskett, Ryan Porterfield                                                *
  *  *                                                                                                *
  *  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software  *
  *  * and associated documentation files (the "Software"), to deal in the Software without           *
