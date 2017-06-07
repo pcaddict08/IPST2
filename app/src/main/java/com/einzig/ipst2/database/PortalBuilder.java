4484be8afe261d0c95860d0d219c04652ade1fbd
Steven Foskett <steven.foskett@gmail.com>
2017-06-07 17:28:06 -0500

    Fixed get query for portals between

--- app/src/main/java/com/einzig/ipst2/database/PortalBuilder.java
+++ app/src/main/java/com/einzig/ipst2/database/PortalBuilder.java
@@ -103,7 +103,7 @@ abstract class PortalBuilder<P extends PortalSubmission> {
     Vector<P> getPortalsByDate(String dateKey, Date fromDate, Date toDate) {
         String fromDateStr = dateFormatter.format(fromDate);
         String toDateStr = dateFormatter.format(toDate);
-        return getPortals("? BETWEEN ? AND ?", new String[]{dateKey, fromDateStr, toDateStr});
+        return getPortals(dateKey + " BETWEEN ? AND ?", new String[]{fromDateStr, toDateStr});
     }
 
     /**
