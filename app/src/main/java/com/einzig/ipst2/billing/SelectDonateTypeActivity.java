3d5b6c5b52331954f639c91794953ac4a82cce62
Steven Foskett <steven.foskett@gmail.com>
2017-07-26 21:45:35 -0500

    More Billing Things
    More billing things.

--- app/src/main/java/com/einzig/ipst2/billing/SelectDonateTypeActivity.java
+++ app/src/main/java/com/einzig/ipst2/billing/SelectDonateTypeActivity.java
@@ -25,6 +25,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,6 +40,16 @@ public class SelectDonateTypeActivity extends AppCompatActivity {
     Button monthlysub_selectdonatetypeactivity;
     @BindView(R.id.onetime_selectdonatetypeactivity)
     Button onetime_selectdonatetypeactivity;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
